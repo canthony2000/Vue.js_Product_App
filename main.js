692ead71d5f81eb77b3b4c96141c95dbc04fbca2
Corey Anthony <canthony@techtonic.com>
2019-07-01 11:58:21 -0600

    Add review form.

--- main.js
+++ main.js
@@ -53,9 +53,10 @@ props: {
         <button
           v-on:click="removeFromCart"
         >
-          Remove
+          Remove Tt
         </button>
       </div>
+      <productReview></productReview>
     </div>
   `,
   data() {
@@ -107,6 +108,45 @@ props: {
         return "Free"
       }
       return 2.99
+    }
+  },
+})
+
+Vue.component('productReview', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" placeholder="name">
+    </p>
+    
+    <p>
+      <label for="review">Review:</label>      
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+    
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+        
+    <p>
+      <input type="submit" value="Submit">  
+    </p>    
+
+  </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
     }
   },
 })
